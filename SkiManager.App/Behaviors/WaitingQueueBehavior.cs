f62377c141ab2354545313b315a4dae8878ccbcc
Tim Vinkemeier <timvinkemeier@live.de>
2016-01-09 18:18:42 +0100

    Added WaitingQueueBehavior.

---
+++ SkiManager.App/Behaviors/WaitingQueueBehavior.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+using System.Runtime.Serialization;
+using Newtonsoft.Json;
+using SkiManager.App.Interfaces;
+using SkiManager.Engine;
+
+namespace SkiManager.App.Behaviors
+{
+    public sealed class WaitingQueueBehavior : ReactiveBehavior, IWaitingQueue
+    {
+        [JsonProperty]
+        public int MaxQueueSize { get; set; }
+        public int QueueSize => _queue.Count;
+        public bool HasWaitingEntities => QueueSize > 0;
+        public IObservable<Entity> WaitingEntityArrived => _waitingEntityArrivedSubject.AsObservable();
+
+        [JsonProperty]
+        private List<Entity> _queue = new List<Entity>();
+        private readonly Subject<Entity> _waitingEntityArrivedSubject = new Subject<Entity>();
+
+        protected override void Loaded(BehaviorLoadedEventArgs args)
+        {
+            args.TrackSubscription(ChildEnter.Subscribe(OnChildEnter));
+        }
+
+        protected override void Destroyed()
+        {
+            _waitingEntityArrivedSubject.Dispose();
+        }
+
+        private void OnChildEnter(ChildEnterEngineEventArgs args)
+        {
+            if (_queue.Count >= MaxQueueSize)
+            {
+                // no space
+                args.EnteringChild.SetParent(args.OldParent);
+            }
+
+            args.EnteringChild.IsEnabled = false;
+            _queue.Add(args.EnteringChild);
+            _waitingEntityArrivedSubject.OnNext(args.EnteringChild);
+        }
+
+        public IReadOnlyList<Entity> GetDisabledEntitiesFromQueue(int count = 1) => _queue.Take(count).ToList().AsReadOnly();
+    }
+}
---
+++ SkiManager.App/Interfaces/IWaitingQueue.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using SkiManager.Engine;
+
+namespace SkiManager.App.Interfaces
+{
+    public interface IWaitingQueue : IHasEntity
+    {
+        int MaxQueueSize { get; }
+
+        int QueueSize { get; }
+
+        bool HasWaitingEntities { get; }
+
+        IObservable<Entity> WaitingEntityArrived { get; }
+
+        IReadOnlyList<Entity> GetDisabledEntitiesFromQueue(int count = 1);
+    }
+}
